a10e71cddc1aadedeac53f0dc78d48156a377130
sergeylyashko <slyashko@mail.ru>
2022-08-15 21:08:14 +0300

    add color settings page

---
+++ src/main/java/com/gllllepulla/plugin/highlighter/AsnColorSettingsPage.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2022 Sergey Lyashko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.gllllepulla.plugin.highlighter;
+
+import com.gllllepulla.plugin.AsnFileType;
+import com.intellij.openapi.editor.colors.TextAttributesKey;
+import com.intellij.openapi.fileTypes.SyntaxHighlighter;
+import com.intellij.openapi.options.colors.AttributesDescriptor;
+import com.intellij.openapi.options.colors.ColorDescriptor;
+import com.intellij.openapi.options.colors.ColorSettingsPage;
+import com.intellij.openapi.util.NlsContexts;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+import java.util.Map;
+
+public class AsnColorSettingsPage implements ColorSettingsPage {
+
+    private static final AttributesDescriptor[] DESCRIPTORS = new AttributesDescriptor[]{
+            new AttributesDescriptor("Key", AsnSyntaxHighlighter.KEY),
+            new AttributesDescriptor("Separator", AsnSyntaxHighlighter.SEPARATOR),
+            new AttributesDescriptor("Value", AsnSyntaxHighlighter.VALUE),
+            new AttributesDescriptor("Bad value", AsnSyntaxHighlighter.BAD_CHARACTER)
+    };
+
+    @Nullable
+    @Override
+    public Icon getIcon() {
+        return AsnFileType.ICON_FILE;
+    }
+
+    @NotNull
+    @Override
+    public SyntaxHighlighter getHighlighter() {
+        return new AsnSyntaxHighlighter();
+    }
+
+    @Override
+    public @NonNls @NotNull String getDemoText() {
+        return """
+                TEXT
+                """;
+    }
+
+    @Nullable
+    @Override
+    public Map<String, TextAttributesKey> getAdditionalHighlightingTagToDescriptorMap() {
+        return null;
+    }
+
+    @Override
+    public AttributesDescriptor @NotNull [] getAttributeDescriptors() {
+        return DESCRIPTORS;
+    }
+
+    @Override
+    public ColorDescriptor @NotNull [] getColorDescriptors() {
+        return ColorDescriptor.EMPTY_ARRAY;
+    }
+
+    @Override
+    public @NotNull @NlsContexts.ConfigurableName String getDisplayName() {
+        return "Asn.1";
+    }
+}
