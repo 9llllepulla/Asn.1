be600a3d666f2f529cf9004f0f825b5d07de9f0a
sergeylyashko <slyashko@mail.ru>
2022-09-21 22:07:16 +0300

    added code style settings

---
+++ src/main/java/com/gllllepulla/plugin/formatter/AsnCodeStyleSettingsProvider.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2022 Sergey Lyashko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.gllllepulla.plugin.formatter;
+
+import com.gllllepulla.plugin.AsnLanguage;
+import com.intellij.application.options.CodeStyleAbstractConfigurable;
+import com.intellij.application.options.CodeStyleAbstractPanel;
+import com.intellij.application.options.TabbedLanguageCodeStylePanel;
+import com.intellij.psi.codeStyle.CodeStyleConfigurable;
+import com.intellij.psi.codeStyle.CodeStyleSettings;
+import com.intellij.psi.codeStyle.CodeStyleSettingsProvider;
+import com.intellij.psi.codeStyle.CustomCodeStyleSettings;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class AsnCodeStyleSettingsProvider extends CodeStyleSettingsProvider {
+
+    @Override
+    @Nullable
+    public CustomCodeStyleSettings createCustomSettings(CodeStyleSettings settings) {
+        return new AsnCodeStyleSettings(settings);
+    }
+
+    @Nullable
+    @Override
+    public String getConfigurableDisplayName() {
+        return "Asn";
+    }
+
+    @NotNull
+    public CodeStyleConfigurable createConfigurable(@NotNull CodeStyleSettings settings, @NotNull CodeStyleSettings modelSettings) {
+        return new CodeStyleAbstractConfigurable(settings, modelSettings, this.getConfigurableDisplayName()) {
+            @Override
+            protected CodeStyleAbstractPanel createPanel(CodeStyleSettings settings) {
+                return new AsnCodeStyleMainPanel(getCurrentSettings(), settings);
+            }
+        };
+    }
+
+    private static class AsnCodeStyleMainPanel extends TabbedLanguageCodeStylePanel {
+
+        public AsnCodeStyleMainPanel(CodeStyleSettings currentSettings, CodeStyleSettings settings) {
+            super(AsnLanguage.INSTANCE, currentSettings, settings);
+        }
+    }
+
+}
