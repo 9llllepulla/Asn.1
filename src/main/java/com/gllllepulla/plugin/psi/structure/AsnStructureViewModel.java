84a687cb96cd346fe97b4dcd5de05bdaacb6f852
sergeylyashko <slyashko@mail.ru>
2022-09-21 21:37:58 +0300

    added structure view navigation

---
+++ src/main/java/com/gllllepulla/plugin/psi/structure/AsnStructureViewModel.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2022 Sergey Lyashko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.gllllepulla.plugin.psi.structure;
+
+import com.gllllepulla.plugin.psi.AsnProperty;
+import com.intellij.ide.structureView.StructureViewModel;
+import com.intellij.ide.structureView.StructureViewModelBase;
+import com.intellij.ide.structureView.StructureViewTreeElement;
+import com.intellij.ide.util.treeView.smartTree.Sorter;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.psi.PsiFile;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class AsnStructureViewModel extends StructureViewModelBase implements StructureViewModel.ElementInfoProvider {
+
+    public AsnStructureViewModel(@Nullable Editor editor, @NotNull PsiFile psiFile) {
+        super(psiFile, editor, new AsnStructureViewElement(psiFile));
+    }
+
+    @Override
+    public Sorter @NotNull [] getSorters() {
+        return new Sorter[]{Sorter.ALPHA_SORTER};
+    }
+
+    @Override
+    public boolean isAlwaysShowsPlus(StructureViewTreeElement element) {
+        return false;
+    }
+
+    @Override
+    public boolean isAlwaysLeaf(StructureViewTreeElement element) {
+        return element.getValue() instanceof AsnProperty;
+    }
+}
